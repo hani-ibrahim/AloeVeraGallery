2fa4230f31159be58479a9146620aab61fd735be
Hani Ibrahim <hani.ibrahim.me@gmail.com>
2019-11-03 16:03:18 +0100

    Adding empty projects

---
+++ AloeVeraGallery/AloeVeraGallery.h
@@ -0,0 +1,19 @@
+//
+//  AloeVeraGallery.h
+//  AloeVeraGallery
+//
+//  Created by Hani on 03.11.19.
+//  Copyright © 2019 Hani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AloeVeraGallery.
+FOUNDATION_EXPORT double AloeVeraGalleryVersionNumber;
+
+//! Project version string for AloeVeraGallery.
+FOUNDATION_EXPORT const unsigned char AloeVeraGalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AloeVeraGallery/PublicHeader.h>
+
+
---
+++ AloeVeraGalleryExample/AloeVeraGalleryExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AloeVeraGalleryExample
+//
+//  Created by Hani on 03.11.19.
+//  Copyright © 2019 Hani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
